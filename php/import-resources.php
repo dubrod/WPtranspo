b176f5e9716833bb2fccad811f3b30fd3e104325
Wayne Roddy <wayneroddy@Waynes-MacBook-Pro.local>
2015-09-25 17:06:05 -0400

    Update import
    only thing not created was matching templates

--- php/import-resources.php
+++ php/import-resources.php
@@ -7,42 +7,66 @@ class Resources_Section {
 
             $xml = processXML($xmlFileName);
 
-            //get posts
+            $pageData = array();
             $postsData = array();
+
+            //get posts
             foreach ($xml->channel->item as $post) {
                 //if its not a post, break
                 $type = $post->children('wp',true)->post_type;
 
+                $wp = $post->children('wp',true);
+                $id = $post->children('wp',true)->post_id;
+                $pt =       $mysqli->real_escape_string(parseContent((string)$post->title));
+                $it =       $mysqli->real_escape_string(parseContent((string)$post->children('excerpt',true)->encoded));
+                $content =  $mysqli->real_escape_string(parseContent((string)$post->children('content',true)->encoded));
+
+                $template = $_POST["tplDefault"];
+
+                $pub_date =  strtotime((string)$post->pubDate);
+                    if (empty($pub_date)) {$pub_date = strtotime((string)$wp->post_date);}
+
+
                 if($type == "post"){
-                    $wp = $post->children('wp',true);
-                    $id = $post->children('wp',true)->post_id;
-                    $parent = $post->children('wp',true)->post_parent;
-                    $pt =       $mysqli->real_escape_string(parseContent((string)$post->title));
-                    $it =       $mysqli->real_escape_string(parseContent((string)$post->children('excerpt',true)->encoded));
-                    $content =  $mysqli->real_escape_string(parseContent((string)$post->children('content',true)->encoded));
-                    $pub_date =  strtotime((string)$post->pubDate);
-                        if (empty($pub_date)) {
-                            $pub_date = strtotime((string)$wp->post_date);
-                        }
-                    $insert = "INSERT INTO `modx_site_content` (`id`,`pagetitle`,`introtext`,`content`,`parent`,`publishedon`,`published`) VALUES ('".$id."','".$pt."','".$it."','".$content."','".$parent."','".$pub_date."','1')";
+
+                    if($_POST["tplParent"]){
+                        $parent = $_POST["tplParent"];
+                    } else {
+                        $parent = $post->children('wp',true)->post_parent;
+                    }
+
+                    //Insert to DB
+                    $insert = "INSERT INTO `modx_site_content` (`id`,`pagetitle`,`introtext`,`content`,`parent`,`template`,`publishedon`,`published`) VALUES ('".$id."','".$pt."','".$it."','".$content."','".$parent."','".$template."','".$pub_date."','1')";
                     $result = $mysqli->query($insert);
                     if ( $result ) { $postsData[] = $pt;}
                 }
-            }
 
-            $pageData = array();
-            /*get pages
 
-            foreach ($xml->channel->item as $post) {
+
+                //get pages
                 //if its not a page, break
-                $type = $post->children('wp',true)->post_type;
 
                 if($type == "page"){
-                    $p = parseContent((string)$post->title);
-                    //echo $p."<br>";
-                    if ( $p ) { $pageData[] = $p;}
+
+                    if($_POST["tplParent"] && !$_POST["tplPageParent"]){
+                        $parent = $_POST["tplParent"];
+                    } else {
+                        $parent = $post->children('wp',true)->post_parent;
+                    }
+
+                    //Insert to DB
+                    $insert = "INSERT INTO `modx_site_content` (`id`,`pagetitle`,`introtext`,`content`,`parent`,`template`,`publishedon`,`published`) VALUES ('".$id."','".$pt."','".$it."','".$content."','".$parent."','".$template."','".$pub_date."','1')";
+                    $result = $mysqli->query($insert);
+                    if( $result ) {
+                        $pageData[] = $pt;
+                    } else {
+                        //try again with no ID, its might have been taken
+                        $reinsert = "INSERT INTO `modx_site_content` (`pagetitle`,`introtext`,`content`,`parent`,`template`,`publishedon`,`published`) VALUES ('".$pt."','".$it."','".$content."','".$parent."','".$template."','".$pub_date."','1')";
+                        $result_two = $mysqli->query($reinsert);
+                        $pageData[] = $pt;
+                    }
                 }
-            }*/
+            }
 
 
             return '<p><strong>Posts Imported:</strong> '.count($postsData).' | <strong>Pages Imported:</strong> '.count($pageData).'</p>';
